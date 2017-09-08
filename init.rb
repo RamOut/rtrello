d743136303aa40c84fa1d239c73e8d974918b169
Rambabu Duvvuru <rambabu.d@outlook.com>
2017-09-08 16:26:26 -0700

    Initial commit

---
+++ init.rb
@@ -0,0 +1,7 @@
+Redmine::Plugin.register :rtrello do
+  name 'RTrello'
+  author 'Rambabu Duvvuru'
+  description 'This is a plugin for Redmine, which integrates Redmine and Trello.'
+  version '0.0.1'
+  url 'https://github.com/RamOut/rtrello'
+end
